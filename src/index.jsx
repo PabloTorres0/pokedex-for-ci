81d6efd8fab061bb5e1d88cca701e192e71f7f67
PabloTorres0 <pablo.javier.torres.martinez@gmail.com>
2024-04-10 13:23:24 -0600

    my 18  workFlow

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,8 @@
+/* eslint-disable linebreak-style */
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import './styles.css'
 
+// eslint-disable-next-line linebreak-style
 ReactDOM.render(<App />, document.getElementById('app'))
